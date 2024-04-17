6fcb0eaa01e1e718fc6cacb5b69ee29ec2930acc
Roman Kusov <rkusoff@gmail.com>
2024-04-17 21:23:04 +0400

    first commit

---
+++ src/test/java/com/librarymicroservices/rootproject/RootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.librarymicroservices.rootproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
