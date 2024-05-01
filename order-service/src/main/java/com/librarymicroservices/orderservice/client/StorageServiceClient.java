36f8752267ee0b58899724705259e2dedf50c106
Danny Kass <mr.Danny.Kass@gmail.com>
2024-05-01 20:47:26 +0300

    changed get to post in StorageServiceClient

--- order-service/src/main/java/com/librarymicroservices/orderservice/client/StorageServiceClient.java
+++ order-service/src/main/java/com/librarymicroservices/orderservice/client/StorageServiceClient.java
@@ -3,13 +3,14 @@ package com.librarymicroservices.orderservice.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(name = "storage-service", path = "api/storage")
 public interface StorageServiceClient {
-    @GetMapping("/pick_up/{id}")
+    @PostMapping("/pick_up/{id}")
     void pickUp(@PathVariable("id") long bookId);
 
-    @GetMapping("/return/{id}")
+    @PostMapping("/return/{id}")
     void returnBook(@PathVariable("id") long bookId);
 
     @GetMapping("/quantity/{id}")
